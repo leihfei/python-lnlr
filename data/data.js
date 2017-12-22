3e1e8b2f305b8cff8c1d7ce383e036ed5e528a7f
雷洪飞 <leihfei@163.com>
2017-12-22 10:27:08 +0800

    购票个人分析记录

---
+++ data/data.js
@@ -0,0 +1,104 @@
+<script>
+/*<![CDATA[*/
+ var ctx='/otn/';
+ var globalRepeatSubmitToken = null;
+ var global_lang = 'zh_CN';
+ var sessionInit = '\u96F7\u6D2A\u98DE';
+ var isShowNotice = null;
+ var CLeftTicketUrl = 'leftTicket/query';
+ var isTestFlow = null;
+ var isMobileCheck = 'N';
+ var passport_appId = 'otn';
+ var passport_login = 'https://kyfw.12306.cn/passport/web/login';
+ var passport_captcha = 'https://kyfw.12306.cn/passport/captcha/captcha-image';
+ var passport_authuam = 'https://kyfw.12306.cn/passport/web/auth/uamtk';
+ var passport_captcha_check = 'https://kyfw.12306.cn/passport/captcha/captcha-check';
+ var passport_authclient = 'uamauthclient';
+ var passport_loginPage = 'login/init';
+ var passport_okPage = 'index/initMy12306';
+ var passport_proxy_captcha =  'login/init';
+ /*]]>*/
+</script>
+
+<script>
+/*<![CDATA[*/
+ var ctx='/otn/';
+ var globalRepeatSubmitToken = '57f8efe839abf0bd7c2bb208883a8ea6';
+ var global_lang = 'zh_CN';
+ var sessionInit = '\u96F7\u6D2A\u98DE';
+ var isShowNotice = null;
+ var CLeftTicketUrl = null;
+ var isTestFlow = null;
+ var isMobileCheck = null;
+ var passport_appId = 'otn';
+ var passport_login = 'https://kyfw.12306.cn/passport/web/login';
+ var passport_captcha = 'https://kyfw.12306.cn/passport/captcha/captcha-image';
+ var passport_authuam = 'https://kyfw.12306.cn/passport/web/auth/uamtk';
+ var passport_captcha_check = 'https://kyfw.12306.cn/passport/captcha/captcha-check';
+ var passport_authclient = 'uamauthclient';
+ var passport_loginPage = 'login/init';
+ var passport_okPage = 'index/initMy12306';
+ var passport_proxy_captcha =  'login/init';
+ /*]]>*/
+</script>
+
+<a href="/otn/modifyUser/initQueryUserInfo">查看个人信息</a>
+
+"MKvgjnDJN0FJaDyfONmRctKOiiO7p3dMw5SI48MHVfDAwEo%2FeAfvq1fdAuONcjQXPgMPQtqtxIPI%0A%2Bmpi8gGvy6GGFuiQlkst%2Fe7O7hGh%2ByohyXolgO1ABd%2FFPh%2Byomf2lOfmimPUpx%2BVOGXh3Hmh24Pe%0A39YtaqNTWic2DmdN6uiAwk6d5oCE95UoDDtoqCGJSVnNx%2FCltxqXotQ%2FwbXgIf3B1dnNKn8yy1tu%0A1wnmG92CUOB8xmzvlI4wTjO%2BXYJz4u9JzskRchI%3D
+|预订|
+41000K10320U
+|
+K1033
+|XAY|GIW|ZIW|GIW|13:48|16:52|03:04|Y|
+w%2BUl%2BGlmD5k4ZW9Qsx2boQAu8QdspvmPARCM74L8TLH7FPpgfX1SOxeVm4A%3D
+|20171220|
+3
+|
+Y2
+|14|15|0|0||||有|||无||有|有|||||10401030|1413|1"
+"OCLmOF2VSzCG0%2FZP7ZhpvOlru0N8IS6TrOmI2pGiLGG6sG04ejBY5JgWCArqZUYlgKklAn4vvbVj%0ARNS7OfCGmKqvifUmUnVI%2BT1eeQe2YLcDBUqVCyc%2FWxFRb9BItQS8x9vVOPCj3GE34BzaS4QyRvLW%0AVG6%2FBmt6zhA1H%2BwostUTG%2Bf%2FuXLCOXb275KC%2BeCZDwAHqTQyVn2NgCKGU8e57U91l5Zueo56b1NK%0ATs5kEUmEWoJzSB0dKPRAOTyRrr0%2BYf42T5%2B2CfM%3D|预订|93000K150406|K1501|WAR|KMM|ZIW|GIW|15:24|18:24|03:00|Y|k3o%2F7wX6hloNiuLebbUJUNnJpAhsVEAh%2FhjE2nCc1xV900tiXGNO8pviSt8%3D|20171219|3|R1|22|24|0|0||||有|||有||有|有|||||10401030|1413|0"
+"Nl96VkRnEkkMU2ihepp%2BoQey2hZpqwZEA4o2a79mSsvTyI1K0%2Bekg%2BHalMiqjp%2BtjWaygJOOdjo2%0An4jPR3pmTLHKog%2FpunEzBQgWa9rRyjBsJ5LdsfYTuerrV7H4p%2BnUAxOpRJ1utxUbPuw%2FxxTO87jt%0ASEJ6pGYd%2Fy%2B2b1h9YpNs1xZLKJU%2BFFpEcBsOjBfoUlhwUTffcDQUQyXAypBqbjYvveid%2FUA8KqUk%0Aazs%2FY6fCvbxkTBvsZvUTP02WyWYyO3NQHLLrfmg%3D|预订|770000K16707|K167|CQW|KMM|ZIW|GIW|16:46|19:46|03:00|Y|5f3LJ8qY0%2BKLSXyEjjhMN%2B7Dts1Y3EuXFODNguOLQSfPaLBULFwLm9TEAfI%3D|20171221|3|W2|05|06|0|0||||9|||无||有|有|||||10401030|1413|1"
+"oBc6nV7sDuUdp5TBXWY7rK%2FzoBvKhdbY%2BiAxSQpnMP98pE7n8eVdKFNyhDlwmh4w%2BCMPlbaBuNg0%0AaGJD3ho9BiFATjmV6BH%2Fid00Yj%2BKMb43XK%2Bn6AffLktHZ9PNermKjAj3zjL1iOtVoWvusiTD%2BKlj%0Arkkcyn7QAw9sn6mLJe4URYsSGBbn3X7XEVki4VEUyAwvoaKgpl7Ky3v6uWSttXCOtBFwDC%2FEwi3L%0AGfLzeZwzaqhQg%2BJxvZjUu6IGV1Q2yfeMSFqhJ3c%3D|预订|780000K83108|K831|ZIW|SNH|ZIW|GIW|21:53|02:09|04:16|Y|Dm%2FNuStGED4u2DjT5J7nl7Tkb47wLvBx%2B8UAE8GhdVJ4k6JSk0F%2BeuIw5qs%3D|20171221|3|W2|01|02|0|0||||有|||无||有|有|||||10401030|1413|1"
+
+
+-1:. 登录成功，获取glableID
+    https://kyfw.12306.cn/otn/confirmPassenger/initDc
+
+0. 获取用户信息
+
+
+1. 检查订单用户
+url: https://kyfw.12306.cn/otn/confirmPassenger/checkOrderInfo
+type: post
+参数：
+cancel_flag:2  固定的
+bed_level_order_num:000000000000000000000000000000 固定的
+passengerTicketStr:3,0,1,贺文英,1,522327198502152044,13692739079,N_3,0,1,雷宏刚,1,522132198509178511,13692739079,N_3,0,1,王云梅,1,522121197008137427,,N  有规律生成的
+oldPassengerStr:贺文英,1,522327198502152044,1_雷宏刚,1,522132198509178511,1_王云梅,1,522121197008137427,1_
+tour_flag:dc
+randCode:
+whatsSelect:1  搞1
+_json_att:
+REPEAT_SUBMIT_TOKEN:c301541ea94442e68e5e1878b49d330a
+
+2. 检查车次队列
+url: https://kyfw.12306.cn/otn/confirmPassenger/getQueueCount
+type:post
+参数：
+train_date:Thu Dec 21 2017 00:00:00 GMT+0800 (中国标准时间)
+train_no:41000K10320U
+stationTrainCode:K1033
+seatType:3
+fromStationTelecode:ZIW
+toStationTelecode:GIW
+leftTicket:w%2BUl%2BGlmD5k4ZW9Qsx2boQAu8QdspvmPARCM74L8TLH7FPpgfX1SOxeVm4A%3D
+purpose_codes:00
+train_location:Y2
+_json_att:
+REPEAT_SUBMIT_TOKEN:57f8efe839abf0bd7c2bb208883a8ea6
+以上数据在车次中拿到，除了00,还不知道是什么
+
+
+最终请求确认订单为：
+url: 'https://kyfw.12306.cn/otn/confirmPassenger/confirmSingle'
+
